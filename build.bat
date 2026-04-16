b827bf9fc37332d67f90f2d3a4a9aae5d5a5055d
Isabella Basso <isabellabdoamaral@gmail.com>
2026-04-16 20:21:13 -0300

    fix windows assimp build output and refresh library

--- build.bat
+++ build.bat
@@ -3,19 +3,24 @@
 setlocal EnableDelayedExpansion
 
 if not exist assimp (
-	git clone --revision 95f09deaaed342b5f4ac6aa0eb5ad747c476f78b https://github.com/assimp/assimp --depth=1
+	git clone --revision 95f09deaaed342b5f4ac6aa0eb5ad747c476f78b https://github.com/assimp/assimp --depth=1 || exit /b 1
 )
 
 set binaries_dir=build
 
 echo Configuring build...
-cmake -S assimp -B %binaries_dir% -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF -DASSIMP_INSTALL_PDB=OFF -DUSE_STATIC_CRT=ON -DASSIMP_BUILD_USD_IMPORTER=ON -DASSIMP_WARNINGS_AS_ERRORS=OFF
+cmake -S assimp -B %binaries_dir% -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF -DASSIMP_INSTALL_PDB=OFF -DUSE_STATIC_CRT=ON -DASSIMP_BUILD_USD_IMPORTER=ON -DASSIMP_WARNINGS_AS_ERRORS=OFF || exit /b 1
 
 echo Building project...
-cmake --build %binaries_dir% --config Release
+cmake --build %binaries_dir% --config Release || exit /b 1
 
-copy /y build\windows\lib\Release\assimp-vc143-mt.lib assimp.lib
+if not exist "%binaries_dir%\lib\Release\assimp-*.lib" (
+    echo ERROR: Could not find the assimp Windows library in %binaries_dir%\lib\Release.
+    exit /b 1
+)
+for %%F in ("%binaries_dir%\lib\Release\assimp-*.lib") do set "ASSIMP_LIB=%%~fF"
 
-popd bindgen
+REM DUMBAI: Publish the Windows artifact under the filename the Odin bindings import.
+copy /y "%ASSIMP_LIB%" libassimp.lib >nul || exit /b 1
 
-echo "Build completed successfully!"
+echo Build completed successfully!
